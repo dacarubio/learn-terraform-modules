652006625ac475b57f09e1960a8e90728809e078
dacarubio <38148369+dacarubio@users.noreply.github.com>
2021-05-24 15:16:51 +1200

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -9,18 +9,3 @@ output "ec2_instance_public_ips" {
   description = "Public IP addresses of EC2 instances"
   value       = module.ec2_instances.public_ip
 }
-
-output "website_bucket_arn" {
-  description = "ARN of the bucket"
-  value       = module.website_s3_bucket.arn
-}
-
-output "website_bucket_name" {
-  description = "Name (id) of the bucket"
-  value       = module.website_s3_bucket.name
-}
-
-output "website_endpoint" {
-  description = "Domain name of the bucket"
-  value       = module.website_s3_bucket.website_endpoint
-}
